f94e3a633e5ff069886cc708ce2a1965d9161882
MikeSoperskiy <154427588+MikeSoperskiy@users.noreply.github.com>
2025-12-24 12:48:25 +0300

    Add uninstall method to IPC API

--- src/preload.js
+++ src/preload.js
@@ -3,7 +3,7 @@ const { contextBridge, ipcRenderer } = require('electron');
 contextBridge.exposeInMainWorld('electronAPI', {
   checkInstalled: (programId) => ipcRenderer.invoke('check-installed', programId),
   installProgram: (program) => ipcRenderer.invoke('install-program', program),
-  installMultiple: (programs) => ipcRenderer.invoke('install-multiple', programs),
+  uninstallProgram: (program) => ipcRenderer.invoke('uninstall-program', program),
   getSystemInfo: () => ipcRenderer.invoke('get-system-info'),
   addCustomProgram: (programData) => ipcRenderer.invoke('add-custom-program', programData),
   onInstallProgress: (callback) => ipcRenderer.on('install-progress', (event, data) => callback(data))
